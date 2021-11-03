e4ba6f16414cee9a1d79057bd4046d22f6f6e83b
David Claybourne <claybourne.david@gmail.com>
2021-11-03 11:16:22 +0100

    Add thymeleaf doctors view

--- src/main/java/test/springboot/demo/DemoApplication.java
+++ src/main/java/test/springboot/demo/DemoApplication.java
@@ -3,20 +3,28 @@ package test.springboot.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import test.springboot.demo.service.DefaultDoctorService;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 @Controller
 public class DemoApplication {
+
+	@Resource(name = "doctorService")
+	private DefaultDoctorService doctorService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@GetMapping("/")
-	public String rootIndex() {
+	public String rootIndex(Model model) {
+		model.addAttribute("doctors", doctorService.getAllDoctors());
 		return "root";
 	}
 }
